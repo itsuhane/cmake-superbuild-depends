22a00b29d9d3498f06fedb3a0414a7c817462902
jinyu li <ljy.swimming@qq.com>
2019-05-07 21:11:53 +0800

    Create SuperBuildDepends.cmake

---
+++ cmake/Modules/SuperBuildDepends.cmake
@@ -0,0 +1,25 @@
+include(FetchContent)
+
+FetchContent_Declare(
+  super-build-depends
+  GIT_REPOSITORY https://github.com/itsuhane/cmake-superbuild-depends.git
+  GIT_TAG        dfc114b
+)
+FetchContent_GetProperties(super-build-depends)
+if(NOT super-build-depends_POPULATED)
+  message(STATUS "Fetching SuperBuildDepends scripts")
+  FetchContent_Populate(super-build-depends)
+  message(STATUS "Fetching SuperBuildDepends scripts - done")
+endif()
+
+if(NOT COMMAND superbuild_option)
+  function(superbuild_option option_name)
+    include("${super-build-depends_SOURCE_DIR}/options/${option_name}.cmake")
+  endfunction()
+endif()
+
+if(NOT COMMAND superbuild_depend)
+  function(superbuild_depend depend_name)
+    include("${super-build-depends_SOURCE_DIR}/depends/${depend_name}.cmake")
+  endfunction()
+endif()
