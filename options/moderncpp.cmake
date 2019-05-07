bb8924b88f28bf0a5cd9f62a62b3ccef70fd9ed0
jinyu li <ljy.swimming@qq.com>
2019-05-07 19:56:10 +0800

    Create moderncpp.cmake

---
+++ options/moderncpp.cmake
@@ -0,0 +1,12 @@
+if(NOT TARGET options::moderncpp)
+  add_library(options::moderncpp INTERFACE IMPORTED GLOBAL)
+  if(TARGET options::cpp17)
+    target_link_libraries(options::moderncpp INTERFACE options::cpp17)
+  elseif(TARGET options::cpp14)
+    target_link_libraries(options::moderncpp INTERFACE options::cpp14)
+  elseif(TARGET options::cpp11)
+    target_link_libraries(options::moderncpp INTERFACE options::cpp11)
+  else()
+    message(FATAL_ERROR "options::moderncpp depends on options::cpp11 or above."
+  endif()
+endif()
