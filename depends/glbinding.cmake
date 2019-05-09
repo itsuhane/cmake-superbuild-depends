c7ba257bc375ad0db51acd3b3010b6efdb509cff
itsuhane <itsuhane@gmail.com>
2019-05-09 18:58:49 +0800

    Add options::modern-cpp dependency for depends::glbinding.

--- depends/glbinding.cmake
+++ depends/glbinding.cmake
@@ -1,4 +1,7 @@
 if(NOT TARGET depends::glbinding)
+  if(NOT TARGET options::modern-cpp)
+    message(FATAL_ERROR "depends::glbinding expects options::modern-cpp")
+  endif()
   FetchContent_Declare(
     depends-glbinding
     GIT_REPOSITORY https://github.com/cginternals/glbinding.git
@@ -15,7 +18,7 @@ if(NOT TARGET depends::glbinding)
   add_subdirectory(${depends-glbinding_SOURCE_DIR} ${depends-glbinding_BINARY_DIR})
   find_package(glbinding REQUIRED PATHS "${depends-glbinding_SOURCE_DIR}" NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
   add_library(depends::glbinding INTERFACE IMPORTED GLOBAL)
-  target_link_libraries(depends::glbinding INTERFACE glbinding::glbinding glbinding::glbinding-aux)
+  target_link_libraries(depends::glbinding INTERFACE glbinding::glbinding glbinding::glbinding-aux options::modern-cpp)
   set(depends-glbinding-source-dir ${depends-glbinding_SOURCE_DIR} CACHE INTERNAL "" FORCE)
   set(depends-glbinding-binary-dir ${depends-glbinding_BINARY_DIR} CACHE INTERNAL "" FORCE)
   mark_as_advanced(depends-glbinding-source-dir)
